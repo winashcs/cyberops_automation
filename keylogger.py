56f35d74bf16a814b8824c62c0134e5fec26af50
winashcs <ashwinarun.c@gmail.com>
2024-07-06 17:57:11 +0530

    Update README.md

--- keylogger.py
+++ keylogger.py
@@ -1,31 +1,53 @@
 import tkinter as tk
-import keyboard
+from tkinter import messagebox
 import os
 
-def record_keyboard():
-    r = keyboard.record(until="escape")
-    t = keyboard.get_typed_strings(r)
-    x = "".join(t)
-    save_to_file(x)
+class KeyboardRecorder:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Keyboard Recorder")
 
-def save_to_file(text):
-    script_dir = os.path.dirname(__file__)  # Absolute path of the script's directory
-    file_path = os.path.join(script_dir, "record.txt")
-    with open(file_path, 'a') as f:
-        f.write(text + '\n')
+        self.record_button = tk.Button(self.root, text="Start Recording", command=self.start_recording)
+        self.record_button.pack(pady=20)
 
-# Create the GUI window
-root = tk.Tk()
-root.title("Keyboard Recorder")
+        self.exit_button = tk.Button(self.root, text="Exit", command=self.exit_application)
+        self.exit_button.pack()
 
-# Create a button to start recording
-record_button = tk.Button(root, text="Start Recording", command=record_keyboard)
-record_button.pack(pady=20)
+        self.recording = False
+        self.recorded_text = ""
 
-# Create an exit button
-exit_button = tk.Button(root, text="Exit", command=root.quit)
-exit_button.pack()
+    def start_recording(self):
+        if not self.recording:
+            self.recording = True
+            self.record_button.config(text="Stop Recording")
+            self.root.bind("<Key>", self.on_key_press)
+        else:
+            self.recording = False
+            self.record_button.config(text="Start Recording")
+            self.root.unbind("<Key>")
 
-# Start the GUI main loop
-root.mainloop()
+            if self.recorded_text:
+                self.save_to_file(self.recorded_text)
+                self.recorded_text = ""
+
+    def on_key_press(self, event):
+        if self.recording:
+            self.recorded_text += event.char
+
+    def save_to_file(self, text):
+        script_dir = os.path.dirname(__file__)
+        file_path = os.path.join(script_dir, "record.txt")
+        with open(file_path, 'a') as f:
+            f.write(text + '\n')
+
+    def exit_application(self):
+        if self.recording:
+            messagebox.showwarning("Warning", "Please stop recording before exiting.")
+        else:
+            self.root.destroy()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = KeyboardRecorder(root)
+    root.mainloop()
 
