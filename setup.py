d009ca63c85f06cbc6d32d3a28123992dda31de3
winashcs <ashwinarun.c@gmail.com>
2024-07-10 12:41:53 +0530

    1.0

---
+++ setup.py
@@ -0,0 +1,20 @@
+import cx_Freeze
+import sys
+import os 
+
+base = None
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+os.environ['TCL_LIBRARY'] = r"C:\Users\ashwi\AppData\Local\Programs\Python\Python312\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\ashwi\AppData\Local\Programs\Python\Python312\tcl\tk8.6"
+
+executables = [cx_Freeze.Executable("ca.py", base=base, icon="icon.ico")]
+
+cx_Freeze.setup(
+    name="CyberOps Automation",
+    options={"build_exe": {"packages": ["tkinter", "os"], "include_files": ["icon.ico", "images"]}},
+    version="1.0",
+    description="A software for automating cybersecurity operations.",
+    executables=executables
+)
